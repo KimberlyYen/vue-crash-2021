bee94c31088e8308cf4c5503e277c5dce39168ae
KimberlyYen <87320011+KimberlyYen@users.noreply.github.com>
2021-11-10 00:11:13 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,101 @@
+// 44:05
+
+<template>
+  <div class="container">
+    <Header title="Task Tracker"/>
+    <Tasks :tasks="tasks" />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+      task: []
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'A',
+        day: '2021-11-09',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'B',
+        day: '2021-11-10',
+        reminder: true,
+      },
+      {
+        is: 3,
+        text: 'C',
+        day: '2021-11-11',
+        reminder: false,
+      }
+    ]
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500&display=swap');
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    font-family: 'Ubuntu', sans-serif;
+  }
+  .container {
+    max-width: 500px;
+    margin: 30px auto;
+    overflow: auto;
+    min-height: 300px;
+    border: 1px solid steelblue;
+    padding: 30px;
+    border-radius: 5px;
+  }
+  .btn {
+    display: inline-block;
+    background: black;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    margin: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    font-size: 15px;
+    font-family: 'Ubuntu', sans-serif;
+    font-weight: 500;
+}
+</style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
